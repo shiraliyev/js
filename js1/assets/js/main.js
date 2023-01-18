98e032f06eeaced7f47f6a0fd55f234dc77c0519
unknown <https://github.com/shiraliyev/text1.git>
2023-01-18 16:42:13 +0300

    js version 1.0

---
+++ js1/assets/js/main.js
@@ -0,0 +1,23 @@
+//////////////// 1.0
+// function getRndInteger(min, max) {
+//   return Math.floor(Math.random() * (max - min)) + min;
+// }
+//////////////////// 1.2
+// console.log(Math.floor(Math.random() * 100));
+
+
+///////////////////// 2.0
+//  var firstname= prompt ("istifadecinin adi");
+// var subname= prompt ("istifadecinin soyadi");
+//  console.log  ((" istifadecinin adi ve soyadi: " )+ firstname  + subname);
+
+
+//////////////////////////// 3.0
+// var firstname= prompt ("istifadecinin adi");
+// var subname= prompt ("istifadecinin soyadi");
+// if (firstname=="Asim"){
+//     console.log  (" Xos geldin Asim veziyyet" );
+// }
+// else{
+// console.log  ((" istifadecinin adi ve soyadi: " )+ firstname +" " + subname);
+// }
